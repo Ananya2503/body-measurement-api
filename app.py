4359395a7592e93bf14fb110d6458e9bd567698e
Ananya Butwong <ananya.butwong@gmail.com>
2022-03-04 12:16:38 +0700

    get distance between point and circumference

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 # import necessary libraries
+import numpy as np
 from tf_bodypix.api import load_model, download_model, BodyPixModelPaths
 from pathlib import Path
 from keras.preprocessing.image import save_img
 import cv2 as cv
 from measure import *
 
-num = 2
+num = 5
 FRONT = 'front'
 SIDE = 'side'
+# 43 79 70 88
 
 # setup input and output path
 def setupPath():
@@ -23,53 +25,52 @@ def loadModel() :
 
 # get prediction result
 def predict():
-    # scale = 50
     front_img = cv.imread(f'./image/{FRONT}-{num}.jpg')
     side_img = cv.imread(f'./image/{SIDE}-{num}.jpg')
-    # height = int(front_img.shape[0] * scale / 100)
-    # width = int(front_img.shape[1] * scale / 100)
     height = 800
     width = 800
     front_img = cv.resize(front_img, (width, height))
     side_img = cv.resize(side_img, (width,height))
     front_result = bodypix.predict_single(front_img)
     side_result = bodypix.predict_single(side_img)
-    print("Prediction finish")
 
     # mask
     front_simple_mask = getSimpleMask(front_result, FRONT)
     side_simple_mask = getSimpleMask(side_result, SIDE)
-    # getColorMask(front_result, front_simple_mask, FRONT)
-    # getColorMask(front_result, side_simple_mask, SIDE)
+    getColorMask(front_result, front_simple_mask, FRONT)
+    getColorMask(front_result, side_simple_mask, SIDE)
 
     # get user height
     result_front_img = cv.imread(f'{output_path}/front-simple-mask.jpg')
     result_side_img = cv.imread(f'{output_path}/side-simple-mask.jpg')
-    user_height_pixel_front, max_coor_front, min_coor_front = getHeightInPixel(result_front_img, width, height)
-    user_height_pixel_side, max_coor_side, min_coor_side = getHeightInPixel(result_side_img, width, height)
+    user_height_pixel_front, max_coor_front = getHeightInPixel(result_front_img, width, height)
+    user_height_pixel_side, max_coor_side = getHeightInPixel(result_side_img, width, height)
 
     # get body part position
-    shoulder_front_position, chest_front_position, waist_front_position, hip_front_position = getBodyProportion(result_front_img, width, height, user_height_pixel_front, max_coor_front, min_coor_front)
-    shoulder_side_position, chest_side_position, waist_side_position, hip_side_position = getBodyProportion(result_side_img, width, height, user_height_pixel_side, max_coor_side, min_coor_side)
-    print(shoulder_front_position, chest_front_position, waist_front_position, hip_front_position)
-    print(shoulder_side_position, chest_side_position, waist_side_position, hip_side_position)
-    return user_height_pixel_front
+    shoulder_front_position, chest_front_position, waist_front_position, hip_front_position = getBodyProportion(user_height_pixel_front, max_coor_front)
+    shoulder_side_position, chest_side_position, waist_side_position, hip_side_position = getBodyProportion(user_height_pixel_side, max_coor_side)
+
+    # measure
+    shoulder, chest, waist, hip = measure([shoulder_front_position, shoulder_side_position],
+            [chest_front_position, chest_side_position],
+            [waist_front_position, waist_side_position],
+            [hip_front_position, hip_side_position],
+            width)
+    return shoulder, chest, waist, hip
 
 # simple mask
 def getSimpleMask(result, pose):
     mask = result.get_mask(threshold=0.5)
     save_img(f'{output_path}/{pose}-simple-mask.jpg', mask)
-    print("Simple mask finish")
     return mask
 
 # color mask
 def getColorMask(result, mask, pose):
     color_mask = result.get_colored_part_mask(mask)
     save_img(f'{output_path}/{pose}-color-mask.jpg', color_mask)
-    print('color mask finish')
 
 if __name__ == '__main__':
     output_path = setupPath()
     bodypix = loadModel()
-    user_height_pixel = predict()
-    print(user_height_pixel)
+    shoulder, chest, waist, hip = predict()
+    print(shoulder, chest, waist, hip)
--- measure.py
+++ measure.py
@@ -1,5 +1,5 @@
-import cv2 as cv
-from keras.preprocessing.image import save_img
+from PIL import Image
+from math import pi, sqrt
 
 # find user height (pixel)
 def getHeightInPixel(image, width, height):
@@ -13,31 +13,76 @@ def getHeightInPixel(image, width, height):
                     max_height = [j, i]
                 if min_height[1] < i:
                     min_height = [j, i]
-    # circle_img = image
-    # cv.circle(circle_img, (max_height[0], max_height[1]), 20, (255, 0, 0), -1)
-    # cv.circle(circle_img, (max_height[0], min_height[1]), 20, (255, 0, 0), -1)
-    # cv.line(circle_img, (max_height[0], max_height[1]), (max_height[0], min_height[1]), (255, 0, 0), 15)
-    # save_img(f'output/test-height.jpg', circle_img)
-    return min_height[1] - max_height[1], max_height, min_height
+    return min_height[1] - max_height[1], max_height
 
 # get body proportion
-def getBodyProportion(image, width, height, user_height_pixel, max_coor, min_coor):
+def getBodyProportion(user_height_pixel, max_coor):
     section_height = int(user_height_pixel / 8)
-    # circle_img = image
-    # for i in range(9):
-    #     y =  max_coor[1] + (section_height * i)
-    #     cv.line(circle_img, (0, y), (width - 1, y), (255, 0, 0), 15)
+
     # shoulder
-    # cv.line(circle_img, (0, int(max_coor[1] + section_height + (section_height / 2))), (width - 1, int(max_coor[1] + section_height + (section_height / 2))), (255, 0, 0), 15)
     shoulder = int(max_coor[1] + section_height + (section_height / 2))
+
     # chest
-    # cv.line(circle_img, (0, max_coor[1] + (section_height * 2)), (width - 1, max_coor[1] + (section_height * 2)), (255, 0, 0), 15)
     chest = max_coor[1] + (section_height * 2)
+
     # waist
-    # cv.line(circle_img, (0, max_coor[1] + (section_height * 3)), (width - 1, max_coor[1] + (section_height * 3)), (255, 0, 0), 15)
     waist = max_coor[1] + (section_height * 3)
+
     # hip
-    # cv.line(circle_img, (0, int(max_coor[1] + (3 * section_height) + (section_height / 2))), (width - 1, int(max_coor[1] + (3 * section_height) + (section_height / 2))), (255, 0, 0), 15)
     hip = int(max_coor[1] + (3 * section_height) + (section_height / 2))
-    # save_img(f'output/test-body-8-section.jpg', circle_img)
+
     return shoulder, chest, waist, hip
+
+def measure(shoulder_point, chest_point, waist_point, hip_point, width):
+    front_img = Image.open(f'output/front-color-mask.jpg')
+    side_img = Image.open(f'output/side-color-mask.jpg')
+
+    # shoulder (only front)
+    shoulder = getDistant(front_img, shoulder_point[0], width)
+
+    # chest
+    chest_front = getDistant(front_img, chest_point[0], width)
+    chest_side = getDistant(side_img, chest_point[1], width)
+    chest = getPerimeter(chest_front, chest_side)
+
+    # waist
+    waist_front = getDistant(front_img, waist_point[0], width)
+    waist_side = getDistant(side_img, waist_point[1], width)
+    waist = getPerimeter(waist_front, waist_side)
+
+    # hip
+    hip_front = getDistant(front_img, hip_point[0], width)
+    hip_side = getDistant(side_img, hip_point[1], width)
+    hip = getPerimeter(hip_front, hip_side)
+   
+    return shoulder, chest, waist, hip
+
+
+def getDistant(image, point, width):
+    R_BASE = range(128, 204)
+    G_BASE = range(203, 256)
+    B_BASE = range(64, 152)
+
+    border_point = [0, 0] # [left, right]
+    count = 0
+    px = image.load()
+
+    for i in range(width):
+        (r, g, b) = px[i, point]
+        if (r in R_BASE) and (g in G_BASE) and (b in B_BASE):
+            if border_point[0] == 0 or (border_point[0] != 0 and count > 50):
+                border_point[0] = i
+            elif border_point[0] != 0:
+                border_point[1] = i
+            count = 0
+        else:
+            count += 1
+    distance = sqrt(pow((border_point[1] - border_point[0]), 2))
+    return int(distance)
+
+def getPerimeter(front_point, side_point):
+    front_distance = int(front_point / 2)
+    side_distance = int(side_point / 2)
+
+    perimeter = 2 * pi * sqrt((pow(front_distance, 2) + pow(side_distance, 2)) / 2)
+    return int(perimeter)
